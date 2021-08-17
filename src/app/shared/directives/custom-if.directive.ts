40d7cc7ab297104315c74b45b348927ea1b3bffe
Arkdio <arcadiolg2@gmail.com>
2021-08-17 13:47:25 -0500

    añadida directiva que destruye

--- src/app/shared/directives/custom-if.directive.ts
+++ src/app/shared/directives/custom-if.directive.ts
@@ -1,10 +1,17 @@
-import { Directive } from '@angular/core';
+import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
 
 @Directive({
   selector: '[customIf]'
 })
 export class CustomIfDirective {
-
-  constructor() { }
+  @Input() set customIf(condicion:boolean){
+    if(condicion){
+      this.viewContainer.createEmbeddedView(this.templateRef); 
+    }else{
+      this.viewContainer.clear(); 
+    }
+  }
+  constructor(private templateRef:TemplateRef<HTMLElement>, 
+              private viewContainer:ViewContainerRef) { }
 
 }
